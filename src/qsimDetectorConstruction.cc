85f8a177fee472659329cad30e89c0c5cbf16cd4
sudipbhattarai <bhatsudi@isu.edu>
2024-10-30 13:41:03 -0400

    Implemented colored visualization

--- src/qsimDetectorConstruction.cc
+++ src/qsimDetectorConstruction.cc
@@ -3,6 +3,9 @@
 #include "qsimDetector.hh"
 #include "qsimScintDetector.hh"
 #include "G4SDManager.hh"
+#include "G4LogicalVolume.hh"
+#include "G4VisAttributes.hh"
+#include "G4Colour.hh"
 
 #include "G4GDMLParser.hh"
 
@@ -16,50 +19,72 @@ qsimDetectorConstruction::qsimDetectorConstruction(const G4GDMLParser& parser): 
 
     // Retrieve Auxiliary Information for sensitive detector
     const G4GDMLAuxMapType* auxmap = fParser.GetAuxMap();
-    G4cout << "Found " << auxmap->size()
-        << " volume(s) with auxiliary information." << G4endl 
-        << G4endl;
+    G4cout << G4endl << "Found " << auxmap->size()
+        << " volume(s) with auxiliary information." << G4endl;
 
+    // Loop over the auxiliary information, attach it to the volumes and print it
     for(G4GDMLAuxMapType::const_iterator iter=auxmap->begin(); iter!=auxmap->end(); iter++){
         G4cout << "Volume " << ((*iter).first)->GetName()
-            << " has the following list of auxiliary information: " << G4endl 
-            << G4endl;
+            << " has the following list of auxiliary information: " << G4endl ;
         for (G4GDMLAuxListType::const_iterator vit=(*iter).second.begin(); vit!=(*iter).second.end(); vit++){
             G4cout << "--> Type: " << (*vit).type
-            << " Value: " << (*vit).value << G4endl;
+            << ", Value: " << (*vit).value << G4endl;
         }
-  }
 
-  G4cout << G4endl;
-
-    // The same as above, but now we are looking for
-    // sensitive detectors setting them for the volumes
-    
-    for(G4GDMLAuxMapType::const_iterator iter=auxmap->begin(); iter!=auxmap->end(); iter++){
-        G4cout << "Volume " << ((*iter).first)->GetName()
-            << " has the following list of auxiliary information: "
-            << G4endl << G4endl;
         for (G4GDMLAuxListType::const_iterator vit=(*iter).second.begin(); vit!=(*iter).second.end();vit++){
-            if ((*vit).type=="SensDet"){
+            // Sensitive detector (SensDet) = name
+            if ((*vit).type=="SensDet" && (*vit).value=="PhotoCathode"){
                 G4cout << "Attaching sensitive detector " << (*vit).value
                     << " to volume " << ((*iter).first)->GetName()
-                    <<  G4endl << G4endl;
+                    <<  G4endl;
             
                 G4VSensitiveDetector* mydet = SDman->FindSensitiveDetector((*vit).value);
                 
-                if(mydet){
-                G4LogicalVolume* myvol = (*iter).first;
-                myvol->SetSensitiveDetector(mydet);
-                }
-                else{
-                G4cout << (*vit).value << " detector not found" << G4endl;
+                if (mydet){
+                    G4LogicalVolume* myvol = (*iter).first;
+                    myvol->SetSensitiveDetector(mydet);
+                } else{
+                    G4cout << (*vit).value << " detector not found" << G4endl;
                 }
             }
+
+            // Color = name
+            if ((*vit).type == "Color") {
+                G4Colour colour(1.0,1.0,1.0);
+                if (G4Colour::GetColour((*vit).value, colour)) {
+
+                    G4cout << "Setting color to " << (*vit).value << "." << G4endl;
+
+                    G4VisAttributes visAttribute(colour);
+                    ((*iter).first)->SetVisAttributes(visAttribute);
+
+                } else {
+                G4cout << "Colour " << (*vit).value << " is not known." << G4endl;
+                }
+            }
+
+            // Alpha = float between 0 and 1
+            if ((*vit).type == "Alpha") {
+                G4Colour colour(1.0,1.0,1.0);
+                const G4VisAttributes* visAttribute_old = ((*iter).first)->GetVisAttributes();
+
+                if (visAttribute_old != nullptr)
+                colour = visAttribute_old->GetColour();
+
+                G4Colour colour_new(
+                    colour.GetRed(), 
+                    colour.GetGreen(),
+                    colour.GetBlue(),
+                    std::atof((*vit).value.c_str()));
+                G4VisAttributes visAttribute_new(colour_new);
+                ((*iter).first)->SetVisAttributes(visAttribute_new);
+            }
         }
+        G4cout << G4endl;
     }
 }
-                                                                               
-qsimDetectorConstruction::~qsimDetectorConstruction(){;}                        
+
+qsimDetectorConstruction::~qsimDetectorConstruction(){;}
 
 G4VPhysicalVolume* qsimDetectorConstruction::Construct() {
   return fParser.GetWorldVolume();
